e0ce1f76f5687cc536aec273e827d845de1c02d3
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-08 00:28:18 -0300

    feat(ui): create sign in page and template for authentication pages

--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -1,8 +1,61 @@
+import { Button } from '@/components/Button'
+import * as InputGroup from '@/components/InputGroup'
+
+import styles from './styles.module.css'
+
 export function SignIn() {
   return (
     <>
       <title>Login - MeuBanco</title>
-      <h1>Sign-in Page</h1>
+
+      <div className={styles.container}>
+        <header className={styles.heading}>
+          <h1 className={styles.headingTitle}>
+            Bem vindo <span>:)</span>
+          </h1>
+          <p className={styles.headingSubtitle}>Entre na sua conta</p>
+        </header>
+
+        <form className={styles.signInForm}>
+          <InputGroup.Root>
+            <InputGroup.Label htmlFor="username">Usuário</InputGroup.Label>
+            <InputGroup.Input inputId="username" />
+          </InputGroup.Root>
+
+          <InputGroup.Root>
+            <InputGroup.Label htmlFor="password">Senha</InputGroup.Label>
+            <InputGroup.Input
+              type="password"
+              inputId="password"
+            />
+          </InputGroup.Root>
+
+          <div className={styles.signInFormActions}>
+            <InputGroup.Root>
+              <InputGroup.Label htmlFor="remember">
+                <InputGroup.Input
+                  type="checkbox"
+                  inputId="remember"
+                />
+                Lembrar dispositivo
+              </InputGroup.Label>
+            </InputGroup.Root>
+
+            <a href="#">Esqueceu sua senha?</a>
+          </div>
+
+          <Button
+            type="submit"
+            appearance="primary"
+          >
+            Entrar
+          </Button>
+        </form>
+
+        <p className={styles.signUpLink}>
+          Não tem conta? <a href="#">Faça uma agora</a>
+        </p>
+      </div>
     </>
   )
 }
--- src/templates/AuthTemplate/index.tsx
+++ src/templates/AuthTemplate/index.tsx
@@ -1,5 +1,35 @@
 import { Outlet } from 'react-router'
 
+import styles from './styles.module.css'
+
+import logoLightImage from '@/assets/logo-ligh.svg'
+import signInImage from '@/assets/sign-in.webp'
+
 export function AuthTemplate() {
-  return <Outlet />
+  return (
+    <div className={styles.fullContainer}>
+      <div className={styles.illustration}>
+        <div className={styles.siteLogo}>
+          <img
+            src={logoLightImage}
+            width={200}
+            height={43}
+            alt="MeuBanco"
+          />
+        </div>
+
+        <div className={styles.figure}>
+          <img
+            src={signInImage}
+            sizes="800px"
+            alt="Uma mulher sorrindo e segurando o seu cartão de crédito"
+          />
+        </div>
+      </div>
+
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+    </div>
+  )
 }
