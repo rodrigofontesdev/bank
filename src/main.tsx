decf07a14f81a6a04726eec650904be592e81187
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-07 22:48:43 -0300

    feat(style): create design tokens and configure webfonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from './router.tsx'
 
+import '@/styles/global.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router />
