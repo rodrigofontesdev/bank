10e1c8c7c5538fa5cd5064af9c9a37f6a8127de8
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-07 23:17:02 -0300

    feat(ui): create component for form inputs

---
+++ src/components/InputGroup/index.tsx
@@ -0,0 +1,41 @@
+import styles from './styles.module.css'
+
+type RootProps = {
+  children: React.ReactNode
+}
+
+type LabelProps = {
+  children: React.ReactNode
+} & React.ComponentProps<'label'>
+
+type InputProps = {
+  inputId: string
+} & Omit<React.ComponentProps<'input'>, 'id' | 'name'>
+
+function Root({ children }: RootProps) {
+  return <div className={styles.inputGroup}>{children}</div>
+}
+
+function Label({ children, ...props }: LabelProps) {
+  return (
+    <label
+      className={styles.inputLabel}
+      {...props}
+    >
+      {children}
+    </label>
+  )
+}
+
+function Input({ inputId, ...props }: InputProps) {
+  return (
+    <input
+      id={inputId}
+      name={inputId}
+      className={styles.inputControl}
+      {...props}
+    />
+  )
+}
+
+export { Root, Label, Input }
