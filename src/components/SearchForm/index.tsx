4a67deff5129750e785bb4cad18cf3e93f384a01
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-08 21:21:26 -0300

    feat(ui): create component for search form

---
+++ src/components/SearchForm/index.tsx
@@ -0,0 +1,30 @@
+import iconSearch from '@/assets/icon-search.svg'
+
+import styles from './styles.module.css'
+
+export function SearchForm() {
+  return (
+    <form>
+      <div className={styles.inputGroup}>
+        <span className={styles.inputPrepend}>
+          <img
+            src={iconSearch}
+            width={20}
+            height={20}
+            alt=""
+            aria-hidden
+          />
+        </span>
+
+        <input
+          type="search"
+          placeholder="Buscar"
+          id="search"
+          name="search"
+          aria-label="Buscar"
+          className={styles.inputControl}
+        />
+      </div>
+    </form>
+  )
+}
