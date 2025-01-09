1953aacdb41e33a276d86c618a8ebeb5c453e046
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-09 19:57:33 -0300

    feat(ui): create component for shortcuts

---
+++ src/components/ShortcutCard/index.tsx
@@ -0,0 +1,27 @@
+import { Box } from '../Box'
+
+import styles from './styles.module.css'
+
+type ShortcutCardProps = {
+  title: string
+  amount: string
+  tag?: string
+  appearance?: 'default' | 'tagged'
+}
+
+export function ShortcutCard({ title, amount, tag, appearance }: ShortcutCardProps) {
+  const appearanceClass = appearance === 'tagged' ? styles.tagged : ''
+
+  return (
+    <Box>
+      <article className={styles.card}>
+        <header className={styles.subject}>
+          <h2>{title}</h2>
+          <p>{amount}</p>
+        </header>
+
+        {tag && <p className={`${styles.tag} ${appearanceClass}`}>{tag}</p>}
+      </article>
+    </Box>
+  )
+}
