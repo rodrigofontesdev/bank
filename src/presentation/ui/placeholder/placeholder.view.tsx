7867655ba32c6cf91639a7e3576d3d5bb11791ac
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-01-09 22:33:03 -0300

    feat(ui): create component for navigation placeholder

---
+++ src/presentation/ui/placeholder/placeholder.view.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css'
+
+export function Placeholder() {
+  return (
+    <div className={styles.container}>
+      <p>Placeholder de navegação</p>
+    </div>
+  )
+}
